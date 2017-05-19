8d358a1e887b26dab1dba375e2325c13092b112e
Thuan Tran <oda1234@uw.edu>
2017-05-19 12:32:15 -0700

    Created Global and Card Class. Created method header for Dealer and Player

---
+++ Dealer.py
@@ -0,0 +1,24 @@
+import numpy as np
+import Card
+class Dealer:
+
+    """""
+    This is the constructor for the Dealer class
+    It will have an array of cards the dealer have
+    An array of card that dealer use to distribute the card
+    A number of deck to know how many deck to use
+    """""
+    def __init__(self, player, numberOfDeck):
+        # Assume that maximum number of card is 22
+        self.numberOfCard= np.zeros(22)
+        self.listOfPlayer = player
+        self.theDeck = Card(numberOfDeck)
+
+    def collectChip(self):
+        pass
+
+    def payOutChip(self):
+        pass
+
+
+
